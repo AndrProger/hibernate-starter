e4397edc34f8004f9164d4f4651597b1f1cc0203
stari <starinenko2251@gmail.com>
2023-05-19 13:21:10 +0600

    2. Logging. Файл log4j.xml

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -5,55 +5,48 @@ import com.dmdev.convertor.BirthdayConverter;
 import com.dmdev.entity.Birthday;
 import com.dmdev.entity.Role;
 import com.dmdev.entity.User;
+import com.dmdev.until.HibernateUtil;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class HibernateRunner {
+
+    private static final Logger log= LoggerFactory.getLogger(HibernateRunner.class);
     public static void main(String[] args) throws SQLException {
-        Configuration configuration = new Configuration();
-//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-//        configuration.addAnnotatedClass(User.class);
-        configuration.addAttributeConverter(new BirthdayConverter());
-        configuration.registerTypeOverride(new JsonBinaryType());
-        configuration.configure();
-
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session=sessionFactory.openSession()
-        ){
-            session.beginTransaction();
-//            User user = User.builder()
-//                    .username("ivan32@gmail.com")
-//                    .firstName("Ivan")
-//                    .lastName("Ivanov")
-//                    .birthDate(new Birthday(LocalDate.of(2000, 1,19)))
-//                    .info("""
-//                            {
-//                                "name":"Ivan",
-//                                "id":25
-//                            }
-//                            """)
-//                    .role(Role.ADMIN)
-//                    .build();
-//
-//            session.delete(user);
 
 
-            User user1 = session.get(User.class,"ivan@gmail.com");
-            User user2 = session.get(User.class,"ivan@gmail.com");
-           // System.out.println(user1);
-            //System.out.println(user2);
-            user1.setLastName("petrov");
-           // session.evict(user1);
-           // session.close();
-            session.getTransaction().commit();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory())
+        {
+            User user = User.builder()
+                    .username("Ivan@gmail.com")
+                    .firstName("Ivan")
+                    .lastName("Ivanov")
+                    .build();
+            log.info("User entity is in transient state, object: {}", user);
+            Session session1=sessionFactory.openSession();
+            try(session1) {
+                Transaction transaction = session1.beginTransaction();
+                log.trace("Transaction is created, {}", transaction);
 
+                session1.saveOrUpdate(user);
+                log.trace("User is in persistent state:{} session {}", user,session1);
+
+                session1.getTransaction().commit();
+            }
+            log.warn("User is in detached state:{} session is closed {}", user,session1);
+        } catch (Exception e){
+            log.error("Exception occurred", e);
         }
+
 
     }
 }
